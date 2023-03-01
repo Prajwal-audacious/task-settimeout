66a35a349b65b95be8b8618e06d86800827071c6
Prajwal-audacious <prajwal.ramanwal@techiegent.com>
2023-03-01 17:15:40 +0530

    Task is done

---
+++ script.js
@@ -0,0 +1,19 @@
+let box = document.querySelector(".container");
+let box2 = document.querySelector(".container_2");
+// console.log(box)
+
+setTimeout(() => {
+  box.style.backgroundColor = "red";
+}, 3000);
+
+let bool = true;
+setInterval(() => {
+  if (bool) {
+    bool = false;
+    box2.style.backgroundColor = "green";
+    
+  } else {
+    bool = true;
+    box2.style.backgroundColor = "blue";
+  }
+}, 1000);
